ea574033c3a224d6f7102469f3bc9f376c6b1999
meh <meh@paranoici.org>
2011-06-20 19:24:30 +0200

    arch: start dependency renaming

--- distros/arch.rb
+++ distros/arch.rb
@@ -1,1 +1,20 @@
 # adapt Distrø dependencies to Arch
+
+before :pack do
+  data = YAML.parse(filesystem.arch_linux.dependencies.to_s).transform
+
+  dependencies.each {|dependency|
+    data.each {|key, value|
+      next unless dependency.in?(Packo::Package.parse(key))
+
+      dependency.name = value
+    }
+  }
+end
+
+__END__
+$$$
+
+$$$ arch_linux/dependencies $$$
+
+python%2: "python2"
